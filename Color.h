e1d46eda6c4da85fe4d37e9bb18ad6938cfbeb90
RoshanSanjeev <115925071+RoshanSanjeev@users.noreply.github.com>
2024-04-27 21:48:17 -0700

    Create Color.h

---
+++ Color.h
@@ -0,0 +1,55 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <GL/freeglut.h>
+
+struct Color {
+private:
+    float r;
+    float g;
+    float b;
+
+public:
+    Color() {
+        r = 0.0f;
+        g = 0.0f;
+        b = 0.0f;
+    }
+
+    Color(float r, float g, float b) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+    void setColor(float r, float g, float b) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+
+    float getR() {
+        return r;
+    }
+
+    float getG() {
+        return g;
+    }
+
+    float getB() {
+        return b;
+    }
+    void setR(float r) {
+        this->r=r;
+    }
+
+    void setG(float g) {
+      this->g=g;
+    }
+
+    void setB(float b) {
+      this->b=b;
+    }
+};
+
+
+#endif
